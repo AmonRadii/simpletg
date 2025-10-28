a18c604319feaa9f1faa7c75063da60a6b4c8b8f
Ruby <80600884+rubywwwilde@users.noreply.github.com>
2025-10-28 11:42:41 +0000

    Updated materializers
    Co-authored-by: AmonRa <AmonRadii@users.noreply.github.com>

--- src/livestore/schema.ts
+++ src/livestore/schema.ts
@@ -1,14 +1,18 @@
 import { Events, makeSchema, Schema, SessionIdSymbol, State } from '@livestore/livestore'
+import { message } from 'telegram/client';
 
 
 export const tables = {
   messages: State.SQLite.table({
     name: 'messages',
     columns: {
-        id: State.SQLite.integer({ primaryKey: true }),
+        // id = peerId/messageId
+        // deleted messages should stay in the table and marked as deleted
+        id: State.SQLite.text({ primaryKey: true }),
+        messageId: State.SQLite.integer(),
         peerId: State.SQLite.text(),
         createdAt: State.SQLite.integer(),
-        text: State.SQLite.json(),
+        messageText: State.SQLite.json(),
         editedAt: State.SQLite.integer({ nullable: true}),
     },
   }),
@@ -16,11 +20,10 @@ export const tables = {
   peers: State.SQLite.table({
     name: 'peers',
     columns: {
-      id: State.SQLite.integer({ primaryKey: true }),
+      id: State.SQLite.text({ primaryKey: true }),
       name: State.SQLite.text(),
       lastName: State.SQLite.text(),
       username: State.SQLite.text(),
-      peerId: State.SQLite.text(),
       unreadCount: State.SQLite.integer(),
       totalCount: State.SQLite.integer(),
       topMessageId: State.SQLite.integer(),
@@ -28,3 +31,97 @@ export const tables = {
     },
   }),
 }
+
+const TextElement = Schema.Struct({
+  text: Schema.String,
+  bold: Schema.Boolean,
+  italic: Schema.Boolean,
+  underline: Schema.Boolean,
+  url: Schema.optional(Schema.String)
+})
+
+type TextElement = typeof TextElement.Type;
+
+const MessageText = Schema.Array(TextElement)
+
+type MessageText = typeof MessageText.Type
+
+export const events = {
+  messageCreateRequest: Events.synced({
+    name: 'v1.MessageCreateRequest',
+    schema: Schema.Struct({
+      messageId: Schema.Number,
+      messageText: MessageText, 
+      createdAt: Schema.Number,
+      peerId: Schema.String   
+    })
+  }),
+  messageCreated: Events.synced({
+    name: 'v1.MessageCreated',
+    schema: Schema.Struct({
+      messageId: Schema.Number,
+      messageText: MessageText,
+      createdAt: Schema.Number,
+      peerId: Schema.String
+    })
+  }),
+  messageEditRequested: Events.synced({
+    name: 'v1.MessageEditRequested',
+    schema: Schema.Struct({
+      messageId: Schema.Number,
+      messageText: MessageText,
+      createdAt: Schema.Number,
+      peerId: Schema.String,
+    })
+  }),
+  messageEdited: Events.synced({
+    name: 'v1.MessageEdited',
+    schema: Schema.Struct({
+      messageId: Schema.Number,
+      messageText: MessageText,
+      createdAt: Schema.Number,
+      peerId: Schema.String,
+    })
+  }),
+  messageDeleteRequested: Events.synced({
+    name: 'v1.MessageDeleteRequested',
+    schema: Schema.Struct({
+      messageId: Schema.Number,
+      peerId: Schema.String,
+      createdAt: Schema.Number,
+    })
+  }),
+  messageDeleted: Events.synced({
+    name: 'v1.MessageDeleted',
+    schema: Schema.Struct({
+      messageId: Schema.Number,
+      peerId: Schema.String,
+      createdAt: Schema.Number,
+    })
+  }),
+  // todo message viewed event - how it is handled in telegram?
+}
+
+
+export const materializers = State.SQLite.materializers(events, {
+  'v1.MessageCreateRequest': ({ messageId, messageText, createdAt, peerId }) => {
+    return tables.messages.insert({ id: `${peerId}/${messageId}`, messageText, createdAt, peerId, messageId  })
+  },
+  'v1.MessageCreated': ({ messageId, messageText, createdAt, peerId }) => {
+    return tables.messages.update({ id: `${peerId}/${messageId}`, messageText, createdAt, peerId }).where({
+      id: `${peerId}/${messageId}`   
+    })
+  },
+  'v1.MessageEditRequested': ({ messageId, messageText, createdAt, peerId }) => {
+    return tables.messages.update({ messageText, editedAt: createdAt }).where({ id: `${peerId}/${messageId}` })
+  },
+  'v1.MessageEdited': ({ messageId, messageText, createdAt, peerId }) => {
+    return tables.messages.update({ messageText, editedAt: createdAt }).where({ id: `${peerId}/${messageId}` })
+  },
+  'v1.MessageDeleteRequested': ({ messageId, peerId }) => {
+    return tables.messages.delete().where({ id: `${peerId}/${messageId}` })
+  },
+  'v1.MessageDeleted': ({ messageId, peerId }) => {
+    return tables.messages.delete().where({ id: `${peerId}/${messageId}` })
+  },
+})
